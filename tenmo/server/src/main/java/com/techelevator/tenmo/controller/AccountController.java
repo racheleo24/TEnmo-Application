be29c15a8e79fbede2392e19146e98182377d235
Rachel Leonard <rcleonar92@gmail.com>
2022-12-14 12:41:57 -0500

    In progress transfer methods

--- tenmo/server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo/server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,28 +1,30 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Account;
+import com.techelevator.tenmo.model.Transfer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.security.Principal;
 
 @RestController
 @RequestMapping (path = "/accounts")
+@PreAuthorize("isAuthenticated()")
 public class AccountController {
 
     @Autowired
     private AccountDao accountDao;
     @Autowired
     private UserDao userDao;
+    @Autowired
+    private TransferDao transferDao;
 
     @GetMapping (path = "/myaccount")
-    @PreAuthorize("isAuthenticated()")
     public Account seeMyAccount(Principal principal){
         int id = userDao.findIdByUsername(principal.getName());
         Account account = accountDao.getAccountByUserId(id);
@@ -30,11 +32,25 @@ public class AccountController {
     }
 
     @GetMapping (path = "/myaccount/balance")
-    @PreAuthorize("isAuthenticated()")
     public BigDecimal seeMyBalance(Principal principal){
         int id = userDao.findIdByUsername(principal.getName());
         Account account = accountDao.getAccountByUserId(id);
         return account.getBalance();
     }
 
+    @PostMapping (path = "/transfer/{username}")
+    public Transfer sendMoney(@PathVariable String username, @RequestBody Transfer transfer, Principal principal) {
+        // get account ids
+        int senderId = userDao.findIdByUsername(principal.getName());
+        int otherId = userDao.findIdByUsername(username);
+        transfer.setInitiatorId(senderId);
+        transfer.setOtherId(otherId);
+
+        // create account objects from ids
+        // update the accounts by the transfer amount using setters
+        // call the update account method with new balances to update in DB
+        // simultaneously create transfer
+
+        BigDecimal transferAmount = transfer.getTransferAmount();
+    }
 }
